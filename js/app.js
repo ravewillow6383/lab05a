71b0ec9a900d82beeacd60727792ccdb35d1a3f0
Raven Robertson <ravewillow6383@gmail.com>
2019-04-05 14:33:45 -0700

    my question one submission

--- js/app.js
+++ js/app.js
@@ -21,6 +21,8 @@ function sum(a, b) { //eslint-disable-line
 
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
+ 
+// This is a comment for my first question, which we did in class.
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -35,10 +37,11 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
 
+
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
